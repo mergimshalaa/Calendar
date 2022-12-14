e7d01953ed4885cca7ec834a6e276e05ad00be4f
Mergim Shala <116654042+mergimshalaa@users.noreply.github.com>
2022-12-14 19:16:10 +0100

    start of showing current month

---
+++ main.js
@@ -0,0 +1,7 @@
+const date = new Date();
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+console.log(months);
+
+/* document.querySelector('.date h1').innerHTML = months[7]; */
